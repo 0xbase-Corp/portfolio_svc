621220774e7228460c4da39e6dd364d75be84be2
0xmzz <zubairzia0@gmail.com>
2024-02-13 01:57:34 -0500

    Refactor bitcoin controller and btc-api the models file in the same way as moralis api files

--- pkg/models/btc-api.go
+++ pkg/models/btc-api.go
@@ -33,18 +33,47 @@ type BitcoinAddressInfo struct {
 	CreatedAt           time.Time `gorm:""`
 }
 
+// TableName overrides the table name used by GORM to `bitcoin_address_info`
+func (BitcoinAddressInfo) TableName() string {
+	return "bitcoin_address_info"
+}
+
 // SaveBitcoinData saves a BitcoinBtcComV1 record and a BitcoinAddressInfo record.
 // btcComV1 is optional and can be nil.
 func SaveBitcoinData(tx *gorm.DB, btcAddressInfo *BitcoinAddressInfo, btcComV1 *BitcoinBtcComV1) error {
-	// Check if btcComV1 is not nil before saving
+	// First, handle the BitcoinBtcComV1 record
 	if btcComV1 != nil {
-		if result := tx.Create(btcComV1); result.Error != nil {
+		var existingBtcComV1 BitcoinBtcComV1
+		// Check if a BitcoinBtcComV1 record already exists for the wallet.
+		result := tx.Where("wallet_id = ?", btcComV1.WalletID).First(&existingBtcComV1)
+
+		if result.Error != nil && result.Error != gorm.ErrRecordNotFound {
+			// Return any other error encountered during the query.
 			return result.Error
+		}
+
+		if result.RowsAffected == 0 {
+			// If the record does not exist, create a new one.
+			if result := tx.Create(btcComV1); result.Error != nil {
+				return result.Error
+			}
+		} else {
+			// If the record exists, update it with the new information.
+			if err := tx.Model(&existingBtcComV1).Updates(btcComV1).Error; err != nil {
+				return err
+			}
+			// Use the ID of the existing record for the BitcoinAddressInfo.
+			btcComV1.BtcAssetID = existingBtcComV1.BtcAssetID
 		}
 	}
 
-	// Check if btcAddressInfo is not nil before saving
+	// Then, handle the BitcoinAddressInfo record
 	if btcAddressInfo != nil {
+		// Ensure btcAddressInfo.BtcAssetID is set correctly, either to the newly created btcComV1 ID or an existing one.
+		if btcComV1 != nil {
+			btcAddressInfo.BtcAssetID = btcComV1.BtcAssetID
+		}
+
 		if result := tx.Create(btcAddressInfo); result.Error != nil {
 			return result.Error
 		}
