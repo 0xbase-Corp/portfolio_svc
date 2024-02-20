8b95d4a50cc04d14008ec64baf79fb2f0505b3aa
0xmzz <zubairzia0@gmail.com>
2024-02-20 10:26:18 -0500

    restoring swagger related lines

--- pkg/controllers/bitcoin-controller.go
+++ pkg/controllers/bitcoin-controller.go
@@ -11,6 +11,21 @@ import (
 	"gorm.io/gorm"
 )
 
+//	@BasePath	/api/v1
+
+// BitcoinController godoc
+//
+// @Summary      Fetch Bitcoin Wallet Information
+// @Description  Retrieves information for a given Bitcoin address using the BTC.com API.
+// @Tags         bitcoin
+// @Accept       json
+// @Produce      json
+// @Param        btc-address  path      string  true  "Bitcoin Address"
+// @Success      200          {object}  struct{ Data models.BitcoinAddressInfo "data"; ErrorCode int "error_code"; ErrNo int "err_no"; Message string "message"; Status string "status" }  "Returns wallet information including transaction history and balance"
+// @Failure      400          {object}  struct{ Error string }  "Bad Request"
+// @Failure      500          {object}  struct{ Error string }  "Internal Server Error"
+// @Router       /portfolio/btc/:btc-address [get]
+
 // BitcoinController handles requests for Bitcoin wallet information.
 func BitcoinController(c *gin.Context, db *gorm.DB) {
 	log.Println("BitcoinController invoked")
