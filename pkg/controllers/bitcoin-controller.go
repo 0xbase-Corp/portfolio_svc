bcce7e647770ef7de4dc7900d4f2fff69a17fc98
0xmzz <zubairzia0@gmail.com>
2024-02-20 10:33:39 -0500

    resolving conflicts

--- pkg/controllers/bitcoin-controller.go
+++ pkg/controllers/bitcoin-controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -51,80 +52,18 @@ func BitcoinController(c *gin.Context, db *gorm.DB) {
 	// Read the response body.
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
+		log.Fatal(err)
+	}
+	fmt.Println("body ", body)
+	var btcResponse models.BtcChainAPI
+	if err := json.Unmarshal(body, &btcResponse); err != nil {
+		log.Fatal(err)
 	}
 
-	// Define a struct to match the JSON response structure from the BTC API.
-	var apiResponse struct {
-		Data struct {
-			Data      models.BitcoinAddressInfo `json:"data"`
-			ErrorCode int                       `json:"error_code"`
-			ErrNo     int                       `json:"err_no"`
-			Message   string                    `json:"message"`
-			Status    string                    `json:"status"`
-		} `json:"data"`
-	}
+	fmt.Println("BTC PResponse ", btcResponse)
+	//TODO: add the data into database
 
-	// Parse the JSON response into the defined struct.
-	if err := json.Unmarshal(body, &apiResponse); err != nil {
-		log.Println("Failed to parse JSON response: " + err.Error())
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse JSON response: " + err.Error()})
-		return
-	}
-	log.Println("Received response from BTC API")
-
-	// Check if a wallet with the given Bitcoin address exists in the global_wallets table
-	var wallet models.GlobalWallet
-	err = db.Where("wallet_address = ?", btcAddress).First(&wallet).Error
-	if err != nil {
-		if err == gorm.ErrRecordNotFound {
-			wallet = models.GlobalWallet{
-				WalletAddress:  btcAddress,
-				BlockchainType: "Bitcoin",
-			}
-			if err := db.Create(&wallet).Error; err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create wallet: " + err.Error()})
-				return
-			}
-		} else {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Database query error: " + err.Error()})
-			return
-		}
-	}
-
-	// Begin a new transaction
-	tx := db.Begin()
-	// Assuming wallet is the GlobalWallet record found or created
-	walletID := wallet.WalletID
-
-	// Initialize btcComV1 and set the WalletID
-	var btcComV1 models.BitcoinBtcComV1
-	btcComV1.WalletID = uint(walletID)
-
-	// Save btcComV1 to the database to get a valid BtcAssetID
-	if err := tx.Create(&btcComV1).Error; err != nil {
-		tx.Rollback()
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save btcComV1 record: " + err.Error()})
-		return
-	}
-
-	// Now that btcComV1 is saved, it has a valid BtcAssetID. Set this ID for BitcoinAddressInfo
-	apiResponse.Data.Data.BtcAssetID = btcComV1.BtcAssetID
-
-	// Proceed to call SaveBitcoinData with the updated BitcoinAddressInfo and btcComV1
-	if err := models.SaveBitcoinData(tx, &apiResponse.Data.Data, &btcComV1); err != nil {
-		tx.Rollback()
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save Bitcoin address info: " + err.Error()})
-		return
-	}
-
-	// Commit the transaction
-	if err := tx.Commit().Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to commit transaction: " + err.Error()})
-		return
-	}
-
-	log.Println("Data saved successfully for address:", btcAddress)
-	c.JSON(http.StatusOK, gin.H{"message": "Data saved successfully"})
+	c.JSON(http.StatusOK, gin.H{
+		"data": btcResponse,
+	})
 }
