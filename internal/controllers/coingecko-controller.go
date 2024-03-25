da290fa707e308075af650c8a1f911edf9364843
0xbuilder0 <asgharali@0xbase.org>
2024-03-26 02:00:51 +0500

    Handle coingecko limit api call

--- internal/controllers/coingecko-controller.go
+++ internal/controllers/coingecko-controller.go
@@ -1,34 +1,52 @@
 package controllers
 
 import (
+	"strings"
+	"time"
+
 	"github.com/0xbase-Corp/portfolio_svc/internal/models"
 	"github.com/0xbase-Corp/portfolio_svc/internal/providers/coingecko"
 	"github.com/0xbase-Corp/portfolio_svc/internal/utils"
 	"gorm.io/gorm"
 )
 
+const maxRetries = 3
+
+// TODO: handle rate limit errors
+// TODO: add timer to call this service
 func FetchAndSaveCoingeckoPriceForCrypto(db *gorm.DB, cryptoID, currency string) error {
 	priceFeedClient := &coingecko.CoingeckoAPI{}
 
-	body, err := priceFeedClient.FetchData(cryptoID, currency)
-	if err != nil {
-		return err
-	}
+	for attempt := 1; attempt <= maxRetries; attempt++ {
+		body, err := priceFeedClient.FetchData(cryptoID, currency)
+		if err != nil {
+			return err
+		}
 
-	resp := coingecko.CryptoResponse{}
+		bodyStr := string(body)
 
-	if err := utils.DecodeJSONResponse(body, &resp); err != nil {
-		return err
-	}
+		if strings.Contains(bodyStr, "You've exceeded the Rate Limit") {
+			time.Sleep(time.Second * time.Duration(2^attempt)) // Exponential backoff
+			continue
+		}
 
-	priceFeed := models.CoingeckoPriceFeed{
-		Name:     cryptoID,
-		Price:    resp[cryptoID][currency],
-		Currency: currency,
-	}
+		resp := coingecko.CryptoResponse{}
 
-	if err := models.UpdateOrCreateCoingeckoPriceFeed(db, &priceFeed); err != nil {
-		return err
+		if err := utils.DecodeJSONResponse(body, &resp); err != nil {
+			return err
+		}
+
+		priceFeed := models.CoingeckoPriceFeed{
+			Name:     cryptoID,
+			Price:    resp[cryptoID][currency],
+			Currency: currency,
+		}
+
+		if err := models.UpdateOrCreateCoingeckoPriceFeed(db, &priceFeed); err != nil {
+			return err
+		}
+
+		return nil
 	}
 
 	return nil
