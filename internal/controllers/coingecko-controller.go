ceb65b42c4d112c7b5ebe1544b608e03a4f8fc3d
0xbuilder0 <asgharali@0xbase.org>
2024-03-26 14:30:33 +0500

    coingecko rate limit is exceeded, return nil not need to update the price feed

--- internal/controllers/coingecko-controller.go
+++ internal/controllers/coingecko-controller.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"strings"
-	"time"
 
 	"github.com/0xbase-Corp/portfolio_svc/internal/models"
 	"github.com/0xbase-Corp/portfolio_svc/internal/providers/coingecko"
@@ -10,44 +9,36 @@ import (
 	"gorm.io/gorm"
 )
 
-const maxRetries = 3
-
-// TODO: handle rate limit errors
-// TODO: add timer to call this service
 func FetchAndSaveCoingeckoPriceForCrypto(db *gorm.DB, cryptoID, currency string) error {
 	priceFeedClient := &coingecko.CoingeckoAPI{}
 
-	for attempt := 1; attempt <= maxRetries; attempt++ {
-		body, err := priceFeedClient.FetchData(cryptoID, currency)
-		if err != nil {
-			return err
-		}
+	body, err := priceFeedClient.FetchData(cryptoID, currency)
+	if err != nil {
+		return err
+	}
 
-		bodyStr := string(body)
-
-		if strings.Contains(bodyStr, "You've exceeded the Rate Limit") {
-			time.Sleep(time.Second * time.Duration(2^attempt)) // Exponential backoff
-			continue
-		}
-
-		resp := coingecko.CryptoResponse{}
-
-		if err := utils.DecodeJSONResponse(body, &resp); err != nil {
-			return err
-		}
-
-		priceFeed := models.CoingeckoPriceFeed{
-			Name:     cryptoID,
-			Price:    resp[cryptoID][currency],
-			Currency: currency,
-		}
-
-		if err := models.UpdateOrCreateCoingeckoPriceFeed(db, &priceFeed); err != nil {
-			return err
-		}
-
+	// check the response body if it contains the rate limit error message then ignore to update or create the price feed
+	if strings.Contains(string(body), "You've exceeded the Rate Limit") {
+		// if rate limit is exceeded, return nil not need to update the price feed
 		return nil
 	}
 
+	resp := coingecko.CryptoResponse{}
+
+	if err := utils.DecodeJSONResponse(body, &resp); err != nil {
+		return err
+	}
+
+	priceFeed := models.CoingeckoPriceFeed{
+		Name:     cryptoID,
+		Price:    resp[cryptoID][currency],
+		Currency: currency,
+	}
+
+	if err := models.UpdateOrCreateCoingeckoPriceFeed(db, &priceFeed); err != nil {
+		return err
+	}
+
 	return nil
+
 }
